bc03b83cabfe2b7a345dde2b059591b1bf9b86e2
Nathaniel McCallum <nathaniel@themccallums.org>
2012-02-10 22:37:16 -0500

    fix macro reference to old memFree function

--- libsc.h
+++ libsc.h
@@ -55,7 +55,7 @@ typedef bool (*scForEach)(void *parent, void *child, void *data);
 #define sc_size_item(m)         sizeof(__typeof__(*m))
 #define sc_size_items(m)        (sc_size(m) / sc_size_item(m))
 #define sc_steal(p, m)          ((__typeof__(m)) _sc_steal(p, m))
-#define sc_destructor_set(m, d) _sc_destructor_set(m, (memFree) d)
+#define sc_destructor_set(m, d) _sc_destructor_set(m, (scFree) d)
 #define sc_ensure(m, t)         ((t*) sc_tag_get(m) \
                                     ? (!strcmp(sc_tag_get(m), __str(t)) \
                                         ? m \
